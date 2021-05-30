27376a3f7f3f89a8187c09c4fd96e9f04ebe1549
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-05-30 19:46:41 +0300

    Added Firearm Version 1.5

--- Models/Firearm.cs
+++ Models/Firearm.cs
@@ -9,7 +9,23 @@ namespace MP5.Models
     public class Firearm
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int MagazineSize { get; set; }
+        public double Weight { get; set; }
+        public double BulletSpeed { get; set; }
+        public double FireDistance { get; set; }
 
-
+        public Firearm()
+        {
+        }
+        public Firearm(int id, string name, int magazineSize, double weight, double bulletSpeed, double fireDistance)
+        {
+            Id = id;
+            Name = name;
+            MagazineSize = magazineSize;
+            Weight = weight;
+            BulletSpeed = bulletSpeed;
+            FireDistance = fireDistance;
+        }
     }
 }
