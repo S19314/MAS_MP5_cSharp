1c6f3b22a469236b5437dac0462c412ceb8fcf5e
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-05-30 13:29:58 +0300

    Initial; Added Soilder and ArmyDbContext aVersion 0.1

---
+++ Models/Soldier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP5.Models
+{
+    public class Soldier
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Rank { get; set; }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
