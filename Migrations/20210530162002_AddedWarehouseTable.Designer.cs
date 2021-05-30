348d098eef509c75908ecf1de6336c10d29a6e5f
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-05-30 19:43:02 +0300

    Create and run Migration for Warehouse Version 1.4

--- Migrations/20210530162002_AddedWarehouseTable.Designer.cs
+++ Migrations/20210530162002_AddedWarehouseTable.Designer.cs
@@ -3,14 +3,16 @@ using MP5.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MP5.Migrations
 {
     [DbContext(typeof(ArmyDbContext))]
-    partial class ArmyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210530162002_AddedWarehouseTable")]
+    partial class AddedWarehouseTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210530162002_AddedWarehouseTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MP5.Migrations
+{
+    public partial class AddedWarehouseTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Soldiers",
+                table: "Soldiers");
+
+            migrationBuilder.RenameTable(
+                name: "Soldiers",
+                newName: "Soilder");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Soilder",
+                table: "Soilder",
+                column: "IdSoldier");
+
+            migrationBuilder.CreateTable(
+                name: "Warehouse",
+                columns: table => new
+                {
+                    IdWarehouse = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: true),
+                    CapacityInBoxes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warehouse", x => x.IdWarehouse);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Warehouse");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Soilder",
+                table: "Soilder");
+
+            migrationBuilder.RenameTable(
+                name: "Soilder",
+                newName: "Soldiers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Soldiers",
+                table: "Soldiers",
+                column: "IdSoldier");
+        }
+    }
+}
